e390518a43208d51bfea6397f9a763c9f0b1674e
Default user <reisportela@gmail.com>
2020-06-06 15:10:35 +0000

    v5

--- 2.R/R_session.R
+++ 2.R/R_session.R
@@ -1,11 +1,15 @@
 ## May 6, 2020
 ## M Portela
 
+rm(list = ls())
+
+#sink("analysis.txt")
+
 version
 
-rm(list = ls())
 setwd("/home/jovyan/2.R")
-getwd()
+
+  getwd()
 
 ## 1. Libraries
 ##https://cran.r-project.org/web/packages/dlookr/vignettes/EDA.html
@@ -19,59 +23,66 @@ library(dlookr)
 library(plm)
 library(stargazer)
 
-#library(ISLR)
-
 ## 2. Read data
 
 data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", header=TRUE)
-
-nlsw88 <- read_dta("../data/nlsw88.dta")
 
 nlswork <- read_dta("../data/nlswork.dta")
 
 #  attach(nlsw88)
 
 df <- na.omit(nlswork)
-attach(df)
+
+  attach(df)
 
 ## 3. Data exploration
 
-## 3.1 Statistics
-summary(nlswork)
+  ## 3.1 Statistics
 
-## 3.2 Graphs
+  summary(nlswork)
+  
+    summary(df)
+  
+    describe(df)
+    
+    freq <- table(south,union)
+    freq
+    prop.table(freq)
+    prop.table(table(union))
+    
+  ## 3.2 Graphs
 
-# Make the histogram
-data %>%
-  filter( price<300 ) %>%
-  ggplot( aes(x=price)) +
-  geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8)
+    # Make the histogram
+    
+      data %>%
+        filter( price<300 ) %>%
+        ggplot( aes(x=price)) +
+        geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8)
 
-## 4. Eploratory Data Analysis
+    hist(ln_wage)
 
-describe(df)
+    png(filename = 'wage_density.png')
+      plot(density(ln_wage))
+    dev.off()
 
-freq <- table(south,union)
-freq
-prop.table(freq)
-prop.table(table(union))
+    plot(density(ln_wage))
+      ggsave("density.eps")
 
-## check: 123
+# 4. REGRESSION ANALYSIS
+      
+  # 4.1 OLS
+      
+      ols1 <- lm(ln_wage ~ union)
+        summary(ols1)
 
-hist(ln_wage)
-png(filename = 'wage_density.png')
-plot(density(ln_wage))
-dev.off()
+  # 4.2 PANEL DATA
+        
+        fe1 <- plm(ln_wage ~ union,data=df,model="within")
+          summary(fe1)  
+        
+        re1 <- plm(ln_wage ~ union,data=df,model="random")
+          summary(re1)  
+          
+stargazer(ols1,fe1,re1,type="text")
 
-plot(density(ln_wage))
-ggsave("density.eps")
-
-ols1 <- lm(ln_wage ~ union)
-
-summary(ols1)
-
-fe1 <- plm(ln_wage ~ union,data=df,model="within")
-
-summary(fe1)  
-
-stargazer(ols1,fe1,type="text")
+#sink()
