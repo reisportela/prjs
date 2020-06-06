8d289737d747a927263cffe71b16352cb3a9f15f
Default user <reisportela@gmail.com>
2020-06-06 13:37:02 +0000

    rev 3

--- 2.R/R_session.R
+++ 2.R/R_session.R
@@ -1,35 +1,77 @@
-## R Session with Jupiter
-## BPLIM Academy
 ## May 6, 2020
 ## M Portela
 
-rm(list = ls())
-getwd()
 version
 
+rm(list = ls())
+setwd("/home/jovyan/2.R")
+getwd()
+
 ## 1. Libraries
-  library(haven)
-  library(ggplot2)
-  library(dplyr)
+##https://cran.r-project.org/web/packages/dlookr/vignettes/EDA.html
+
+
+library(haven)
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(dlookr)
+library(plm)
+library(stargazer)
+
+#library(ISLR)
 
 ## 2. Read data
 
-  data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", header=TRUE)
-  
-  nlsw88 <- read_dta("data/nlsw88.dta")
-  nlswork <- read_dta("data/nlswork.dta")
+data <- read.table("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", header=TRUE)
 
-  attach(nlsw88)
-  
+nlsw88 <- read_dta("../data/nlsw88.dta")
+
+nlswork <- read_dta("../data/nlswork.dta")
+
+#  attach(nlsw88)
+
+df <- na.omit(nlswork)
+attach(df)
+
 ## 3. Data exploration
-  
+
 ## 3.1 Statistics
-  summary(nlswork)
+summary(nlswork)
 
 ## 3.2 Graphs
-  
-  # Make the histogram
-  data %>%
-    filter( price<300 ) %>%
-    ggplot( aes(x=price)) +
-    geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8)
+
+# Make the histogram
+data %>%
+  filter( price<300 ) %>%
+  ggplot( aes(x=price)) +
+  geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8)
+
+## 4. Eploratory Data Analysis
+
+describe(df)
+
+freq <- table(south,union)
+freq
+prop.table(freq)
+prop.table(table(union))
+
+## check
+
+hist(ln_wage)
+png(filename = 'wage_density.png')
+plot(density(ln_wage))
+dev.off()
+
+plot(density(ln_wage))
+ggsave("density.eps")
+
+ols1 <- lm(ln_wage ~ union)
+
+summary(ols1)
+
+fe1 <- plm(ln_wage ~ union,data=df,model="within")
+
+summary(fe1)  
+
+stargazer(ols1,fe1,type="text")
