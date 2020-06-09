125d983da683e0da3bed216ad78ff74c334243aa
reisportela <reisportela@gmail.com>
2020-06-09 18:45:16 +0000

    OCR GESIS

--- 7.OCR/joram.sh
+++ 7.OCR/joram.sh
@@ -8,7 +8,7 @@ pdftotext $ff pdfs/ff_V1.txt ;
 ocrmypdf -l por --sidecar aa2.txt --tesseract-timeout=0 --force-ocr $ff tmp.pdf
 pdfimages -png tmp.pdf pdfs/I ;
 cd pdfs ;
-convert -colorspace gray -fill white -resize 150% -sharpen 0x5 toc -- *.png ff_PNG.pdf ;
+convert -colorspace gray -fill white -resize 100% -sharpen 0x5 toc -- *.png ff_PNG.pdf ;
 rm *.png ;
 parallel --tag -j 2 ocrmypdf -l por --sidecar 'ff_V2.txt' --force-ocr --output-type pdf '{}' '{}' ::: *.pdf ;
 cd .. ;
